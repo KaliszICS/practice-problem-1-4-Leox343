a3459927e05373abb562188836f3e39f7c9a288d
Leox343 <leo.xu6@student.tdsb.on.ca>
2024-09-23 18:30:51 +0000

    f

--- PP1_4.py
+++ PP1_4.py
@@ -10,22 +10,22 @@ def q1():
   print(answer)
 def q2():
   #Write Assignment code here
-  answer = input("Input your first name: ")
+  answer1 = input("Input your first name: ")
   print("Hello " + answer)
 def q3():
   #Write Assignment code here
-  answer = input("Input your first name: ")
-  answer1 = input("Input your last name: ")
+  answer2 = input("Input your first name: ")
+  answer3 = input("Input your last name: ")
   print(f"{answer1} {answer}")
 def q4():
   #Write Assignment code here
-  answer = input("Input a student: ")
-  answerr = input("Input another student: ")
+  answer4 = input("Input a student: ")
+  answer5 = input("Input another student: ")
   print(f"Your students are {answer} and {answerr}")
 #Comment Out the go below when you run your test cases.
 #Do not comment it out when you want to run your code normally
 
-# q1()
-# q2()
-# q3()
-# q4()
+#q1()
+#q2()
+#q3()
+#q4()
